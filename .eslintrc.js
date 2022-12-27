29ecabaae1b36ffcdd102b635c3cf4aa7014545e
wejdan <jode200951@hotmail.com>
2022-12-27 21:25:38 +0300

    example1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
+  parser: '@babel/eslint-parser',
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
 };
